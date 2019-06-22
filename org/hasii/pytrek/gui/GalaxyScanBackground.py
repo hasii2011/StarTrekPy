dad861345c478268fb896c7358fc48d9d5f50781
Humberto Sanchez II <Humberto.A.Sanchez.II@gmail.com>
2019-06-22 13:23:46 -0500

    Fix Galaxy Scan;  Does not need to be sub-classed from a game piece

--- org/hasii/pytrek/gui/GalaxyScanBackground.py
+++ org/hasii/pytrek/gui/GalaxyScanBackground.py
@@ -1,30 +1,42 @@
 
 import pygame
 
+from pygame import Surface
+
+from pygame.sprite import Sprite
+
 from org.hasii.pytrek.Settings import WHITE
 from org.hasii.pytrek.Settings import RED
 
-from org.hasii.pytrek.engine.Intelligence import Intelligence
 from org.hasii.pytrek.objects.Galaxy import Galaxy
-
 from org.hasii.pytrek.objects.Coordinates import Coordinates
 
 from org.hasii.pytrek.gui.GamePiece import GamePiece
-from org.hasii.pytrek.Settings import Settings
+
 from org.hasii.pytrek.engine.Computer import Computer
+from org.hasii.pytrek.engine.Intelligence import Intelligence
 
 X_OFFSET    = 24
 Y_OFFSET    = 16
 
-class GalaxyScanBackground(GamePiece):
+
+class GalaxyScanBackground(Sprite):
     """"""
     def __init__(self, screen: pygame.Surface):
         """"""
 
-        super().__init__(screen, 'images/GalaxyScanBackground.png')
+        super().__init__()
+
+        self.screen: Surface     = screen
+        self.image = pygame.image.load('images/GalaxyScanBackground.png')
 
         self.computer = Computer()
         self.labelFont = pygame.font.Font("fonts/FuturistFixedWidth.ttf", 14)
+
+        self.rect   = self.image.get_rect()
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+        self.x      = float(self.rect.x)
 
     def update(self, galaxy: Galaxy):
         """Displays galaxy content"""
@@ -45,13 +57,12 @@ class GalaxyScanBackground(GamePiece):
                 #
                 # The galaxy was built in Column Major format
                 #
-                quadCoords = Coordinates(j,i)
+                quadCoords = Coordinates(j, i)
                 quadrant   = galaxy.getQuadrant(quadrantCoordinates=quadCoords)
 
                 strValue = self.computer.createValueString(quadrant=quadrant)
-                if quadrant.commanderCount > 0 :
+                if quadrant.commanderCount > 0:
                     label = self.labelFont.render(strValue, 1, RED)
                 else:
                     label    = self.labelFont.render(strValue, 1, WHITE)
                 self.screen.blit(label, (xPos, yPos))
-
