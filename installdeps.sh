47e7df5587a837071aa948fea45e21367aaf427c
Humberto Sanchez II <humberto.a.sanchez.ii@gmail.com>
2020-05-03 19:21:06 -0500

    keep trying

---
+++ installdeps.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#
+# Assumes python 3 is on PATH
+# Assumes you are in a virtual environment
+#
+clear
+pip3 list > /dev/null 2>&1
+STATUS=$?
+
+if [[ ${STATUS} -eq 0 ]] ; then
+    echo "in virtual environment"
+    pip3 install --upgrade pip
+    pip3 install wheel
+    pip3 install pygame python3-albow jsonpickle
+else
+    echo "You are not in a virtual environment"
+
+fi
